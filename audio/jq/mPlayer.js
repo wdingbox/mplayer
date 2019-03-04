adf74e92820456e047caed4e78a59123d6793eed
weiding <weiding@104-188-182-128.lightspeed.tukrga.sbcglobal.net>
2019-03-04 11:52:28 -0500

    reorg

--- audio/jq/mPlayer.js
+++ audio/jq/mPlayer.js
@@ -10,39 +10,41 @@ Date.prototype.addDays = function (days) {
 }
 Date.prototype.addMilliseconds = function (mils) {
 	var date = new Date(this.valueOf());
-	var uuuu=date.getTime();//return in milliseconds value of date. 
+	var uuuu = date.getTime();//return in milliseconds value of date. 
 	date.setTime(uuuu + mils);
 	return date;
 }
-
-function convert_second_to_hhmmss(snd) {
-	var mlis = parseFloat(snd) * 1000.0;
-	var dt0 = new Date(0, 0, 0, 0, 0, 0, 0);
-	var dt = dt0.addMilliseconds(mlis);
-	var yy = dt.getYear().toString().padStart(2, "0");
-	var mn = dt.getMonth().toString().padStart(2, "0");
-	var dd = dt.getDate().toString().padStart(2, "0");
-	var hh = dt.getHours().toString().padStart(2, "0");
-	var mm = dt.getMinutes().toString().padStart(2, "0");
-	var ss = dt.getSeconds().toString().padStart(2, "0");
-	var uu = dt.getMilliseconds().toString().padStart(2, "0");
-	console.log(`${yy}-${mn}-${dd} ${hh}:${mm}:${ss}.${uu}`);
-	return `${hh}:${mm}:${ss}.${uu}`;
-}
-function convert_hhmmss_to_seconds(hhmmss){
-	var arr=hhmmss.split(":");
-	for(var i=0;i<arr.length;i++){
-		arr[i]=parseInt(arr[i]);
+var Util = {
+	convert_second_to_hhmmss: function (snd) {
+		var mlis = parseFloat(snd) * 1000.0;
+		var dt0 = new Date(0, 0, 0, 0, 0, 0, 0);
+		var dt = dt0.addMilliseconds(mlis);
+		var yy = dt.getYear().toString().padStart(2, "0");
+		var mn = dt.getMonth().toString().padStart(2, "0");
+		var dd = dt.getDate().toString().padStart(2, "0");
+		var hh = dt.getHours().toString().padStart(2, "0");
+		var mm = dt.getMinutes().toString().padStart(2, "0");
+		var ss = dt.getSeconds().toString().padStart(2, "0");
+		var uu = dt.getMilliseconds().toString().padStart(2, "0");
+		console.log(`${yy}-${mn}-${dd} ${hh}:${mm}:${ss}.${uu}`);
+		return `${hh}:${mm}:${ss}.${uu}`;
+	},
+	convert_hhmmss_to_seconds: function (hhmmss) {
+		var arr = hhmmss.split(":");
+		for (var i = 0; i < arr.length; i++) {
+			arr[i] = parseInt(arr[i]);
+		}
+		return arr[0] * 3600 + arr[1] * 60 + arr[2];
 	}
-	return arr[0]*3600+arr[1]*60+arr[2];
 }
-convert_second_to_hhmmss(59.123);//second
-convert_second_to_hhmmss(159.123);//second
+
+Util.convert_second_to_hhmmss(59.123);//second
+Util.convert_second_to_hhmmss(159.123);//second
 function show_current_time() {
 	var curTime = gvObj.currentTime;
 	console.log(curTime);
 
-	var hhmmss = convert_second_to_hhmmss(curTime);
+	var hhmmss = Util.convert_second_to_hhmmss(curTime);
 
 	$("#currTime").text(curTime + "=" + hhmmss);
 
@@ -181,7 +183,7 @@ $(function () {
 	$("#hhmmssToDecimal").click(function () {
 		var hhmmss = $("#start_hhmmss").val();
 		console.log(hhmmss);
-		var v = convert_hhmmss_to_seconds(hhmmss);
+		var v = Util.convert_hhmmss_to_seconds(hhmmss);
 		console.log(v);
 		$("#start_float").val(v);
 
