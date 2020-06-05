8622e94a9ad598626ef5c6da09a78792eca5dc5c
weiding <wdingsoft@gmail.com>
2020-06-05 05:00:23 -0400

    ad video

---
+++ video/htm/tmp.json.js
@@ -0,0 +1,34 @@
+////C:\2016-0819\b\svn\tool\test\audio_player\player
+////C:\2016-0819\b\___bigdata\___biblestudy\ciu\courses\Bib5132-Gospels
+//dir1:{file1:{tm1:info1,tm2:info2,,,},file2:{t1:n1,,}},dir2:{f2:{t2:des2}},,,,
+
+var mPlayerResources={
+
+    path:"/Users/weiding/Downloads/",
+    files:{
+    "IMG_0000.MOV":{"00:00:00.0":"weiding prayer","01:53:20.0":"observation1"},
+    "IMG_0001.MOV":{"00:00:00.0":"weiding prayer","01:53:20.0":"observation1"},
+    "IMG_0002.MOV":{"00:00:00.0":"weiding prayer","01:53:20.0":"observation1"},
+    "IMG_0003.MOV":{"00:00:00.0":"weiding prayer","01:53:20.0":"observation1"},
+    "IMG_0004.MOV":{"00:00:00.0":"weiding prayer","01:53:20.0":"observation1"},
+    "IMG_0005.MOV":{"00:00:00.0":"weiding prayer","01:53:20.0":"observation1"},
+   
+    "27-Psa-090.mp3":{"00:00:00.0":"主阿、你世世代代作我们的居所。","01:53:20.0":"observation1"},
+    
+    
+    
+    },/////////////////
+    
+    
+    
+    
+    
+    
+    
+    
+    };///////////////////////////////////////////////////////////
+    
+    /**************
+    {"filename":{"starttime":"des","":""}}
+    
+    ***************/
