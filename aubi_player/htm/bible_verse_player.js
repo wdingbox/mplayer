e46ae6308b1395393757f1a39d44d8b7c8200ce5
wdingsoft <wdingbox@gmail.com>
2021-09-18 08:07:51 -0400

    udpate dat

--- aubi_player/htm/bible_verse_player.js
+++ aubi_player/htm/bible_verse_player.js
@@ -23,11 +23,11 @@ $(function () {
 
 });////////////////////////////////
 
-function play_param_bcv(bcv,txt) {
+function play_param_bcv(bcv, txt) {
     console.log("bcv=", bcv)
     bcv = bcv.replace(/\s/g, "")
     var mat = bcv.match("([0-9a-zA-Z]{3})([0-9]+)[\:]([0-9]+)")
-    if(!mat){
+    if (!mat) {
         return alert("bcv value invalid.")
     }
     console.log(mat)
@@ -36,11 +36,12 @@ function play_param_bcv(bcv,txt) {
     var Vrs = mat[3]
     var audsrc = Audio_Bible_Struct.findAudioUrlFolderPath(Bk, Chp)
 
-    var BibleObj = NIV
+    var BibleObj = VrsAudioRelativePosLen_NIV
     var relativePosi = BibleObj[Bk][Chp][Vrs]
 
-    var dis = `${bcv}  ${audsrc} <br>${txt}`
+    var dis = `${bcv} <br>${txt}`
     $("#playname").html(dis)
+    $("#filename").val(audsrc)
 
     setTimeout(function () {
 
@@ -54,16 +55,19 @@ function play_param_bcv(bcv,txt) {
                 return;
             }
             console.log("maxlen", maxlen)
-            gvObj.currentTime = maxlen * parseFloat(relativePosi)
+            var starttime = parseFloat(maxlen) * parseFloat(relativePosi)
+            //gvObj.currentTime = starttime
             //gvObj.play()
+            $("#start_float").val(starttime)
             console.log(audsrc)
+            $("#start_loop").trigger("click")
         }, 500)
     }, 0)
 }
 
 function gen_bible_table() {
 
-    var BibleObj = NIV
+    var BibleObj = VrsAudioRelativePosLen_NIV
     var tab = ""
     for (let Bk in BibleObj) {
         console.log(Bk)
@@ -71,13 +75,14 @@ function gen_bible_table() {
             //console.log(Bk)
             tab += `<tr><td>${Bk}${Chp}</td><td>`
             for (let Vrs in BibleObj[Bk][Chp]) {
-                var relativePosi = BibleObj[Bk][Chp][Vrs]
+                var relativePosi = BibleObj[Bk][Chp][Vrs][0]
+                var relativeLen = BibleObj[Bk][Chp][Vrs][1]
                 //console.log()
                 var Chp3 = Chp.padStart(3, "0")
                 var src = Audio_Bible_Struct.findAudioUrlFolderPath(Bk, Chp)
                 var bcv = `${Bk}${Chp}:${Vrs}`
 
-                tab += `<a src='${src}' title='${bcv} ${relativePosi}' relativePosi=${relativePosi}> ${Vrs}</a>,`;
+                tab += `<a src='${src}' title='${bcv} bcv='${bcv}' ${relativePosi}' relativePosi=${relativePosi} relativeLen='${relativeLen}' Bok='${Bk}' Chp='${Chp}' Vrs='${Vrs}'> ${Vrs}</a>,`;
             }
             tab = tab.replace(/,$/, "")
             tab += "</td></tr>"
@@ -85,12 +90,22 @@ function gen_bible_table() {
     }
     $("#myAudioFileNameSelect tbody").append(tab).find("a").on("click", function () {
         var audsrc = $(this).attr("src")
-        var relativePosi = $(this).attr("relativePosi")
+
+        var Bok = $(this).attr("Bok")
+        var Chp = $(this).attr("Chp")
+        var Vrs = $(this).attr("Vrs")
+        var bcv = $(this).attr("bcv")
+        var Txt = NIV[Bok][Chp][Vrs]
+
+        var relativePosi = parseFloat($(this).attr("relativePosi"))
+        var relativeLen = parseFloat($(this).attr("relativeLen"))
         $(".hili").removeClass("hili")
         $(this).addClass("hili")
 
         var title = $(this).attr("title")
-        $("#playname").text(title)
+        $("#playname").html(title + "<br>" + Txt)
+
+        $("#filename").val(audsrc)
 
         gvObj = null
         gvObj = document.getElementById('myAudio');
@@ -98,17 +113,26 @@ function gen_bible_table() {
         setTimeout(function () {
             gvObj.src = audsrc
             //gvObj.play()
-            gvObj.pause()
+            //gvObj.pause()
             setTimeout(function () {
-                var maxlen = gvObj.duration;//(audio len in seconds)
+                var maxlen = parseFloat(gvObj.duration);//(audio len in seconds)
                 if (!maxlen) {
                     alert("try again")
                     return;
                 }
                 console.log("maxlen", maxlen)
-                gvObj.currentTime = maxlen * parseFloat(relativePosi)
-                gvObj.play()
+                var starttime = maxlen * relativePosi
+                var duratime = maxlen * relativeLen
+
+
+                //gvObj.currentTime = starttime
+                //gvObj.play()
                 console.log(audsrc)
+
+                $("#start_float").val(starttime)
+                $("#duration_float").val(duratime)
+
+                $("#start_loop").trigger("click")
             }, 500)
         }, 0)
 
